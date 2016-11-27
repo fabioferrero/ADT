4a93f90897a7ba4168c0dd6e780e9d4a6697e538
Fabio Ferrero <fabio.ferrero@icloud.com>
2016-11-27 11:35:27 +0100

    Correct typo on stds.h

--- stds.h
+++ stds.h
@@ -6,7 +6,7 @@ typedef struct fifo * Fifo;
 
 Item    fifo_get(Fifo, int index);
 
-// define a Gederic get function that maps to the correct function
+// define a Generic get function that maps to the correct function
 // depending on the type of the first parameter
 #define get(x, y) _Generic((x), List: list_get, \
                                   Fifo: fifo_get)(x, y)
